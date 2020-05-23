67e8d647f4d6be807dabaf998a9494726ec25c27
arrandimr <61189026+arrandimr@users.noreply.github.com>
2020-05-23 15:13:40 +0700

    Add files via upload

---
+++ koneksisipid.php
@@ -0,0 +1,8 @@
+<?php
+$host = "localhost";
+$username = "id13806450_sipid";
+$password = "Pendataancovid-2019";
+$db = "id13806450_final_project";
+
+$conn = mysqli_connect($host,$username,$password,$db);
+?>
